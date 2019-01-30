a4d579deb19866000e393ef81a8a6c298f9d5d63
qqweea <32316148+qqweea@users.noreply.github.com>
2019-01-30 05:14:27 -0500

    first test build

---
+++ precache-manifest.d0c5d80a8155827134ed71fa1fb35d2b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8dbf72fba67e26215c316b2f6e5646a",
+    "url": "/static/media/logo.a8dbf72f.svg"
+  },
+  {
+    "revision": "624ded49bba48ffa6352a05c28ea4fd4",
+    "url": "/static/media/header.624ded49.jpg"
+  },
+  {
+    "revision": "c260143fa2b9aa28ec4dd17f1c1a8f2a",
+    "url": "/static/media/brush1.c260143f.svg"
+  },
+  {
+    "revision": "22b7dd6a0468b091429a3803e9ffec56",
+    "url": "/static/media/brush.22b7dd6a.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f1f2d834138987714b09",
+    "url": "/static/js/main.f1f2d834.chunk.js"
+  },
+  {
+    "revision": "a3d0d5bdc63d2057cd6c",
+    "url": "/static/js/1.a3d0d5bd.chunk.js"
+  },
+  {
+    "revision": "f1f2d834138987714b09",
+    "url": "/static/css/main.0d4362cb.chunk.css"
+  },
+  {
+    "revision": "496e84e95ade439a0f98df858c277a8f",
+    "url": "/index.html"
+  }
+];
